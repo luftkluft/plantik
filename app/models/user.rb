21b3781c86c99dca3654271f8536e1223ba754fe
luftkluft <luftkluft@gmail.com>
2019-11-08 22:16:24 +0200

    Dev/user: test Bcrypt on Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
+require 'bcrypt'
+
 class User < ApplicationRecord
+  has_secure_password
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -13,4 +16,14 @@ class User < ApplicationRecord
                               message: 'Only emails allowed' }
 
   validates :role, presence: true
+
+  attr_accessor :password
+
+  def authenticate(plaintext_password)
+    if BCrypt::Password.new(encrypted_password) == plaintext_password
+      self
+    else
+      false
+    end
+  end
 end
