73f2712c26169cfde68b5a3b9ec4d47b722a4dec
luftkluft <luftkluft@gmail.com>
2020-01-26 20:15:31 +0200

    Dev/dev: Add empty App component

---
+++ app/javascript/components/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import classes from './app.module.css'
+
+class App extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        App
+      </React.Fragment>
+    );
+  }
+}
+
+export default App
