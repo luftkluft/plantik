1bbcde65bcde6688504747b374e1a043d9bc222b
luftkluft <luftkluft@gmail.com>
2020-02-09 16:49:47 +0200

    Bagfix/dev: return to step6 bugfix heroku

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -5,7 +5,7 @@ class App extends React.Component {
   render() {
     return (
       <div className={classes.grid}>
-        <header className={classes.header}>шапка</header>
+        <header className={classes.header}>шапка reset to step6</header>
         <aside className={classes.aside}>тулбар</aside>
         <content className={classes.content}>содержание</content>
         <footer className={classes.footer}>подвал</footer>
